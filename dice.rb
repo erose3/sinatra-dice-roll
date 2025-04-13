c94c0ac723bbb084a9655d149df236e7af682317
erose3 <eli.rosenthal3@gmail.com>
2025-04-13 17:30:38 +0000

    bullets

--- dice.rb
+++ dice.rb
@@ -5,10 +5,12 @@ line = "="*20
 
 get("/") do
   "<h1>DICE ROLL</h1>
-  <h3><a href = '/dice/2/6'> Roll two 6-sided dice</a></h3>
-  <h3>Roll two 10-sided dice</h3>
-  <h3>Roll one 20-sided dice</h3>
-  <h3>Roll five 4-sided dice</h3>"
+  <ul>
+  <li><a href = '/dice/2/6'> Roll two 6-sided dice</a></li>
+  <li><a href = '/dice/2/10'>Roll two 10-sided dice</a></li>
+  <li><a href = '/dice/1/20'>Roll one 20-sided dice</a></li>
+  <li><a href = '/dice/5/4'>Roll five 4-sided dice</a></li>
+  </ul>"
 
 end
 
@@ -17,10 +19,13 @@ get ("/dice/2/6") do
   second_die = rand(1...6)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-
-  "<h1> 2d6</h1>
-  <p>#{outcome}</p>
+  "<h1>Rolling two 6-sided dice</h1>
+  You rolled:
+  <ul>
+  <li> #{first_die} </li>
+  <li>#{second_die}</li>
+  </ul>
+  <p> For a total of #{sum}.</p>
   <p></p>
   <p><a href = '/' > Return Home 🏠 </a></p>"
 
@@ -32,6 +37,47 @@ get ("/dice/2/10") do
   sum = first_die + second_die
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1> 2d6 </h1>
-  <p> #{outcome} </p>"
+  "<h1> Rolling two 10-sided dice </h1>
+  You rolled:
+  <ul>
+  <li> #{first_die} </li>
+  <li>#{second_die}</li>
+  </ul>
+  <p> For a total of #{sum}.</p>
+  <p></p>
+  <p><a href = '/' > Return Home 🏠 </a></p>"
+end
+
+get ("/dice/1/20") do
+  first_die = rand(1...20)
+  outcome = "You rolled a #{first_die}."
+
+  "<h1> Rolling one 20-sided dice </h1>
+  <p>#{outcome}</p>
+  <p></p>
+  <p><a href = '/' > Return Home 🏠 </a></p>"
+end
+
+get ("/dice/5/4") do
+  first_die = rand(1...4)
+  second_die = rand(1...4)
+  third_die = rand(1...4)
+  fourth_die = rand(1...4)
+  fifth_die = rand (1...4)
+
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die}; for a total of #{sum}."
+
+  "<h1> Rolling five 4-sided dice </h1>
+  You rolled:
+  <ul>
+  <li> #{first_die} </li>
+  <li>#{second_die}</li>
+  <li>#{third_die}</li>
+  <li>#{fourth_die}</li>
+  <li>#{fifth_die}</li>
+  </ul>
+  <p> For a total of #{sum}.</p>
+  <p></p>
+  <p><a href = '/' > Return Home 🏠 </a></p>"
 end
